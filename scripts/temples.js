ad3cf0900d1e417622cdf91b0574c639c9cb7715
TheDarkSide857 <indominusdallin@icloud.com>
2025-05-29 19:40:45 -0700

    Add files via upload

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,10 +3,11 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent =
     "Last Modification: " + document.lastModified;
 
-const hamburgerBtn = document.getElementById("hamburgerBtn");
-const navMenu = document.getElementById("primaryNav");
-
-hamburgerBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
-  hamburgerBtn.classList.toggle("open");
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const nav = document.getElementById('primaryNav');
+    
+hamburgerBtn.addEventListener('click', () => {
+  nav.classList.toggle('open');
+  const isOpen = nav.classList.contains('open');
+  hamburgerBtn.setAttribute('aria-expanded', isOpen);
 });
